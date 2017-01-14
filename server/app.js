62e8bb8d8f097f265ca85326ce9776080a3fa426
Sam <orgillsams@gmail.com>
2017-01-14 21:22:00 +0000

    add mongo logic for individual sport

--- server/app.js
+++ server/app.js
@@ -12,6 +12,9 @@ app.get("/sports", (request, response) => {
   let sports = mongoUtil.sports();
   sports.find().toArray((err,docs) => {
       console.log(JSON.stringify(docs));
+      if(err){
+      response.sendStatus(400);
+    }
       let sportNames = docs.map((sport) => sport.name);
        response.json(sportNames);
  });
@@ -19,73 +22,16 @@ app.get("/sports", (request, response) => {
 
 app.get("/sports/:name", (request, response) => {
   let sportName = request.params.name;
-  console.log("Sport name: ", sportName);
-
-  let sport = {
-            "name" : "Cycling",
-            "goldMedals" : [{
-              "division": "Men's Sprint",
-              "country": "UK",
-              "year": 2012
-            }, {
-              "division": "Woman's Sprint",
-              "country": "Australia",
-              "year": 2012
-            }]
-          }; 
-
-  response.json(sport);
-});
-
-/*let bodyParser = require("body-parser");
-let jsonParser = bodyParser.json();
-
-app.get("/sports", (request, response) => {
-  let sports = mongoUtil.sports();
-  sports.find().toArray((err,docs) => {
-    if(err) {
-      response.sendStatus(400);
-    }
-    console.log(JSON.stringify(docs));
-    let sportNames = docs.map((sport) => sport.name);
-    response.json( sportNames );
-  });
-});
-
-app.get("/sports/:name", (request, response) => {
-  let sportName = request.params.name;
 
   let sports = mongoUtil.sports();
   sports.find({name: sportName}).limit(1).next((err,doc) => {
-    if(err) {
-      response.sendStatus(400);
-    }
-    console.log( "Sport doc: ", doc );
-    response.json(doc);
-  });
-
-});
-
-
-app.post("/sports/:name/medals", jsonParser, (request, response) => {
-  let sportName = request.params.name;
-  let newMedal = request.body.medal || {};
-
-  if(!newMedal.division || !newMedal.year || !newMedal.country){
-    response.sendStatus(400);
-  }
-
-  let sports = mongoUtil.sports();
-  let query = {name: sportName};
-  let update = {$push: {goldMedals: newMedal}};
-
-  sports.findOneAndUpdate(query, update, (err, res) => {
+    console.log(JSON.stringify(doc));
     if(err){
       response.sendStatus(400);
     }
-    response.sendStatus(201);
+      console.log("Sport doc: ", doc);
+      response.json(doc);
   });
 });
-*/
 
 app.listen(8181, () => console.log( "Listening on 8181" ));
