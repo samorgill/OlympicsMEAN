c20de30dc0a18407abc43c7494c3f8b6272e5833
Sam <orgillsams@gmail.com>
2017-01-14 16:21:48 +0000

    added medals table and dynamic routes

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,50 @@
 import angular from 'angular'
-angular.module('olympics', [])
-.controller('sportsController', function($http){
-  //this.sports = ["Weightlifting", "Cycling"];
-  $http.get('/sports').then((response) => {
-    this.sports = response.data;
+import 'angular-ui-router'
+angular.module('olympics', ["ui.router"])
+
+.config(($stateProvider, $urlRouterProvider) => {
+  $urlRouterProvider.otherwise('/sports')
+
+  $stateProvider
+  .state('sports', {
+    url: '/sports',
+    templateUrl: '/sports/sports-nav.html',
+    resolve: {
+      sportsService: function($http){
+        return $http.get('/sports'); 
+      }
+    },
+    controller: function(sportsService){
+      this.sports = sportsService.data;
+    },
+    controllerAs: 'sportsCtrl'
+  })
+  .state('sports.medals', {
+    url: '/:sportName',
+    templateUrl: '/sports/sports-medals.html',
+    resolve: {
+      sportService: function($q){
+        return $q((resolve, reject) => {
+          let sport = {
+            "name" : "Cycling",
+            "goldMedals" : [{
+              "division": "Men's Sprint",
+              "country": "UK",
+              "year": 2012
+            }, {
+              "division": "Woman's Sprint",
+              "country": "Australia",
+              "year": 2012
+            }]
+          };
+          resolve({data: sport});
+        })
+      }
+    },
+    controller: function(sportService){
+      this.sport = sportService.data;
+    },
+    controllerAs: 'sportCtrl'
   })
 })
+
